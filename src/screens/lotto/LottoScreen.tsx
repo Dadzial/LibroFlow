112fd6e4c20e24b65a5f490ed4e55da5d5e74352
Dadzial <duszakdamian034@gmail.com>
2026-03-07 20:41:13 +0100

    fix of expo version

--- src/screens/lotto/LottoScreen.tsx
+++ src/screens/lotto/LottoScreen.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import {lottoStyles} from "./LottoScreen.styles";
 
-export default function lottoScreen() {
+export default function LottoScreen() {
     return (
         <View style={lottoStyles.container}>
             <Text>This is lotto!</Text>
